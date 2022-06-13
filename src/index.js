3eac2a5e2d47ed43aa138676abb8af99ec68a025
Kunj <shahkunj42@gmail.com>
2022-06-13 10:21:39 -0400

    commit tree 74571f08f936e3d2a860aa8b7f6ec50ac4106153

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch(imgUrl)
+.then(resp => resp.json())
+.then(data => {
+    let pictures = data.message
+    pictures.forEach(picture => {
+        let img = document.createElement('img')
+        img.src = picture
+        let div = document.querySelector('div')
+        div.append(img)
+    })
+})
+.catch(e => console.log('You moron'))
+
+fetch(breedUrl)
+.then(res => res.json())
+.then(data => {
+    let message = data.message
+    console.log(data.message)
+    for(const key in message)
+    console.log(key)
+})
